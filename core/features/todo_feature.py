651768f9d13c93c55475f1a26ccd97a383a615d3
Bernardusz <b.christian.nugroho.study@gmail.com>
2025-05-04 14:23:06 +0700

    Base Manager Skeleton

---
+++ core/features/todo_feature.py
@@ -0,0 +1,7 @@
+class ToDoFeature:
+    def __init__(self):
+        pass
+
+class ToDo:
+    def __init__(self):
+        pass
---
+++ core/manager.py
@@ -0,0 +1,7 @@
+from features.notes_feature import NotesFeature, Notes, MathNotes, ScienceNotes, LanguagesNotes
+from features.pomodoro_feature import PomodoroFeature, Pomodoro
+from features.todo_feature import ToDoFeature, ToDo
+
+class Manager:
+    def __init__(self):
+        pass
