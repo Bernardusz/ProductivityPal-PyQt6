671522e585010a762ca26ca97e5dc1fe63237254
Bernardusz <b.christian.nugroho.study@gmail.com>
2025-05-10 14:39:57 +0700

    fixed bugs lol

--- core/features/notes_feature.py
+++ core/features/notes_feature.py
@@ -1,18 +1,37 @@
+from utils.file_helper import generate_id
 class NotesFeature:
     def __init__(self):
-        pass
+        self.notes = {}
+    
+    def add_note(self, title, desc, subject, notes):
+        id = generate_id(title, )
 class Notes:
-    def __init__(self):
-        pass
+    def __init__(self, title, desc, subject, notes):
+        self.title = title
+        self.desc = desc
+        self.subject = subject
+        self.notes = notes
 
 class MathNotes(Notes):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, title, desc, subject, notes):
+        super().__init__(title, desc, subject, notes)
+        self.formula = {}
+    
+    def add_formula(self, name, formula):
+        self.formula[name] = formula
 
 class ScienceNotes(Notes):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, title, desc, subject, notes):
+        super().__init__(title, desc, subject, notes)
+        self.formula = {}
+    
+    def add_formula(self, name, formula):
+        self.formula[name] = formula
 
 class LanguagesNotes(Notes):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, title, desc, subject, notes):
+        super().__init__(title, desc, subject, notes)
+        self.vocab = {}
+
+    def add_vocab(self, word, meaning):
+        self.vocab[word] = meaning
